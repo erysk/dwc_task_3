c3acd6fdac807d8a8a0f927082e3c1dee144d677
vagrant <vagrant@localhost.localdomain>
2020-06-20 04:05:36 +0000

    bookモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_112127) do
+ActiveRecord::Schema.define(version: 2020_06_20_035324) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_18_112127) do
     t.datetime "remember_created_at"
     t.string "name", default: "", null: false
     t.text "introduction"
-    t.bigint "profile_image_id"
+    t.integer "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
