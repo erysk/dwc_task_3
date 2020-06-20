77206b664ac970c6b1df9969b8f426d8aeebab13
vagrant <vagrant@localhost.localdomain>
2020-06-20 04:35:54 +0000

    登録、ログイン後のリダイレクト先変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   root 'home#index'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+
   get 'home/about'
+  resources :users, only: %i(index show edit update)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
